b4902890c246ff7184e2b5c0d81c170c167f38db
Sandesh Kumar K S <sandeshkumar555dreams@gmail.com>
2021-05-08 13:01:03 +0530

    Updates

---
+++ precache-manifest.a0da84bc3b3025974013083479204f59.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744d5904e45c8c2b0fcfb51ebd1c0d15",
+    "url": "/gh-page/index.html"
+  },
+  {
+    "revision": "bde5bef322bb2f5d5961",
+    "url": "/gh-page/static/css/main.10464d77.chunk.css"
+  },
+  {
+    "revision": "9e1c4aceae965d9ca140",
+    "url": "/gh-page/static/js/2.6e204833.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/gh-page/static/js/2.6e204833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde5bef322bb2f5d5961",
+    "url": "/gh-page/static/js/main.040098fd.chunk.js"
+  },
+  {
+    "revision": "d47dc1a2dd8203bd9496",
+    "url": "/gh-page/static/js/runtime-main.e70a94c6.js"
+  },
+  {
+    "revision": "8897aeb643b17c37ab222bf70378423c",
+    "url": "/gh-page/static/media/React_ToDo_List.8897aeb6.webp"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/gh-page/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/gh-page/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/gh-page/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/gh-page/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/gh-page/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/gh-page/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/gh-page/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/gh-page/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/gh-page/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/gh-page/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/gh-page/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/gh-page/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/gh-page/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/gh-page/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/gh-page/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/gh-page/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/gh-page/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/gh-page/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/gh-page/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/gh-page/static/media/typescript.3de182d2.svg"
+  }
+]);
